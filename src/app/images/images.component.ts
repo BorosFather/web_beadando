325c99def6203ed7b246aac2c633a7c560ecd1f0
BorosFather <boros.zoli024@gmail.com>
2023-10-15 15:18:17 +0200

    images oldal kesz(csak szepiteni), calendar kesz(csak szepiteni), chess kesz (belso bordert megcsinalni)

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventListenerObject } from 'rxjs/internal/observable/fromEvent';
 
 @Component({
   selector: 'app-images',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class ImagesComponent {
 
+  ngOnInit(): void {
+    this.all();
+    this.blend();
+    this.sw();
+  }
+
+  showBlend: boolean = true;
+  showSw: boolean = true;
+
+  all() {
+    this.showBlend = true;
+    this.showSw = true;
+  }
+
+  blend() {
+    this.showSw = false;
+    this.showBlend = true;
+  }
+
+  sw() {
+    this.showBlend = false;
+    this.showSw = true;
+  }
+
 }
+
+    
+   
+   
+    
+   
+   
+
+
